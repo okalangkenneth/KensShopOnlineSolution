e80a03b0674c253cdba8b9406cf8d5572db7f926
Kenneth Okalang <okalang.ok@gmail.com>
2022-11-07 15:08:44 +0100

    Add project files.

---
+++ KensShopOnline.Api/Program.cs
@@ -0,0 +1,33 @@
+using KensShopOnline.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//***IMPORTANT INSTRUCTION HERE - MUST CONFIGURE CONNECTION BEFORE RUNNING MIGRATIONS
+builder.Services.AddDbContextPool<KensShopOnlineDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("KensShopOnlineConnection"))
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
